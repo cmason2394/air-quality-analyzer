fbcae90bbfa2a8fe99630c2e253fe9036fbe633d
Cassi <cassi2394@gmail.com>
2026-07-21 09:14:15 -0600

    unit tests for change_time_unit

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -27,3 +27,29 @@ class TestContainsLetter:
 
     def test_contains_letter_single_character_returns_true(self):
         assert mf.contains_letter('a') == True
+
+class TestChangeTimeUnit:
+    """Tests for the change_time_unit function."""
+    def test_change_time_unit_less_than_60_returns_seconds(self):
+        assert mf.change_time_unit(50) == (50, 's')
+    def test_change_time_unit_60_seconds_returns_1_min(self):
+        assert mf.change_time_unit(60) == (1.0, 'min')
+    def test_change_time_unit_90_seconds_returns_1_and_half_min(self):
+        assert mf.change_time_unit(90) == (1.5, 'min')
+    def test_change_time_unit_3600_seconds_returns_1_hrs(self):
+        assert mf.change_time_unit(3600) == (1.0, 'hrs')
+    def test_change_time_unit_5400_seconds_returns_1_and_half_hrs(self):
+        assert mf.change_time_unit(5400) == (1.5, 'hrs')
+    def test_change_time_86400_seconds_returns_1_day(self):
+        assert mf.change_time_unit(86400) == (1.0, 'days')
+    def test_change_time_unit_129600_seconds_returns_1_and_half_days(self):
+        assert mf.change_time_unit(129600) == (1.5, 'days')
+    def test_change_time_unit_0_returns_0_seconds(self):
+        assert mf.change_time_unit(0) == (0, 's')
+    def test_change_time_unit_1_and_half_seconds_returns_1_and_half_seconds(self):
+        assert mf.change_time_unit(1.5) == (1.5, 's')
+    def test_change_time_unit_60_and_half_seconds_returns_1_min(self):
+        assert mf.change_time_unit(60.5) == (1.0, 'min')
+
+    
+    
